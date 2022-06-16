87bcce700b806a6301fd18e9b1cb78eeb1a775a5
WangaiJM <jahkeyjohn@gmail.com>
2022-06-16 22:46:34 +0300

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nation</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <header>
+        <h1><a href="/">nation</a></h1>
+    </header>
+    <section id="main">
+        <div class="content">
+            <h2><a href="">Lorem ipsum dolor sit, amet consectetur adipisicing.</a></h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ab distinctio voluptatibus optio labore modi quod maxime animi, illo ex atque debitis architecto. Iusto, doloribus.
+            </p>
+        </div>
+        <div class="content">
+            <h2><a href="">Lorem ipsum dolor sit, amet consectetur adipisicing.</a></h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ab distinctio voluptatibus optio labore modi quod maxime animi, illo ex atque debitis architecto. Iusto, doloribus.
+            </p>
+        </div>
+        <div class="content">
+            <h2><a href="">Lorem ipsum dolor sit, amet consectetur adipisicing.</a></h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ab distinctio voluptatibus optio labore modi quod maxime animi, illo ex atque debitis architecto. Iusto, doloribus.
+            </p>
+        </div>
+    </section>
+    <footer> 
+        <p>Copyright &copy; 2022</p>
+    </footer>
+</body>
+</html>
